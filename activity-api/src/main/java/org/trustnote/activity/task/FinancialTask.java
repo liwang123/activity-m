aaf74d1a00b1ba2890f533c837847c4140cc1482
zhuxl <zhu_xiaolei_OK@163.com>
2018-06-21 15:19:36 +0800

    1. 定时执行计算任务单线程

---
+++ activity-api/src/main/java/org/trustnote/activity/task/FinancialTask.java
@@ -0,0 +1,77 @@
+package org.trustnote.activity.task;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.trustnote.activity.service.iface.FinancialLockUpService;
+
+/**
+ * @author zhuxl
+ */
+@Component
+@EnableScheduling
+public class FinancialTask {
+    private static final Logger logger = LogManager.getLogger(FinancialTask.class);
+
+    @Autowired
+    private FinancialLockUpService financialLockUpService;
+
+    /**
+     * 每天2:00执行计算在计划收益期间的收益
+     */
+    @Scheduled(cron = "0 0 2 * * ?")
+    public void calculateInComeAmount() {
+        this.financialLockUpService.saveInComeAmount();
+    }
+
+    /**
+     * 每隔5分钟计算周套餐产品的剩余额度
+     */
+    @Scheduled(cron = "0 0/5 * * * ?")
+    public void validationPaymentWeek() {
+        this.financialLockUpService.validationPaymentWeek();
+    }
+
+    /**
+     * 每隔15分钟计算其他套餐产品的剩余额度
+     */
+    @Scheduled(cron = "0 0/20 * * * ?")
+    public void validationPaymentOther() {
+        this.financialLockUpService.validationPaymentWeek();
+    }
+
+
+    /**
+     * 每天0:30执行计算抢购活动结束的lockUpAmount
+     */
+    @Scheduled(cron = "0 30 0 * * ?")
+    public void calculateLockUpAmount() {
+        this.financialLockUpService.saveLockUpAmount();
+    }
+
+
+    /*@Scheduled(cron = "0/3 * * * * ?")
+    public void test1() {
+        FinancialTask.logger.info("每隔3秒定时执行 {}　{}", Thread.currentThread().getId(), Thread.currentThread().getName());
+        try {
+            Thread.sleep(5000);
+        } catch (final InterruptedException e) {
+            e.printStackTrace();
+        }
+        FinancialTask.logger.info("test1执行完毕.");
+    }
+
+    @Scheduled(cron = "0/3 * * * * ?")
+    public void test2() {
+        FinancialTask.logger.info("每隔3秒定时执行 {}　{}", Thread.currentThread().getId(), Thread.currentThread().getName());
+        try {
+            Thread.sleep(2000);
+        } catch (final InterruptedException e) {
+            e.printStackTrace();
+        }
+        FinancialTask.logger.info("test2执行完毕");
+    }*/
+}
--- activity-api/src/main/java/org/trustnote/activity/task/TickerTask.java
+++ activity-api/src/main/java/org/trustnote/activity/task/TickerTask.java
@@ -2,11 +2,8 @@ package org.trustnote.activity.task;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-import org.trustnote.activity.service.iface.FinancialLockUpService;
 
 /**
  * 〈抓取数据task〉
@@ -18,44 +15,7 @@ import org.trustnote.activity.service.iface.FinancialLockUpService;
 @Component
 @EnableScheduling
 public class TickerTask {
-
     private static final Logger logger = LogManager.getLogger(TickerTask.class);
-
-    @Autowired
-    private FinancialLockUpService financialLockUpService;
-
-    /**
-     * 每天2:00执行计算在计划收益期间的收益
-     */
-    @Scheduled(cron = "0 0 2 * * ?")
-    public void calculateInComeAmount() {
-        this.financialLockUpService.saveInComeAmount();
-    }
-
-    /**
-     * 每隔10分钟计算周套餐产品的剩余额度
-     */
-    @Scheduled(cron = "0 0/2 * * * ?")
-    public void validationPaymentWeek() {
-        this.financialLockUpService.validationPaymentWeek();
-    }
-
-    /**
-     * 每隔35分钟计算其他套餐产品的剩余额度
-     */
-    @Scheduled(cron = "0 0/30 * * * ?")
-    public void validationPaymentOther() {
-        this.financialLockUpService.validationPaymentWeek();
-    }
-
-
-    /**
-     * 每天0:30执行计算抢购活动结束的lockUpAmount
-     */
-    @Scheduled(cron = "0 30 0 * * ?")
-    public void calculateLockUpAmount() {
-        this.financialLockUpService.saveLockUpAmount();
-    }
 
     /**
      @Scheduled(cron = "0/2 * * * * ? ")
