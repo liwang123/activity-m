e77f5d6e875cc9e710f0a1550cad7b6e897bb387
zhuxl <zhu_xiaolei_OK@163.com>
2018-06-21 17:39:51 +0800

    1. 修改非规范代码

--- activity-common/src/main/java/org/trustnote/activity/common/utils/SendEmailUtil.java
+++ activity-common/src/main/java/org/trustnote/activity/common/utils/SendEmailUtil.java
@@ -1,51 +1,45 @@
 package org.trustnote.activity.common.utils;
 
-import java.io.IOException;
-        import java.io.UnsupportedEncodingException;
-        import java.security.GeneralSecurityException;
-        import java.util.Date;
-        import java.util.Properties;
+import com.sun.mail.util.MailSSLSocketFactory;
 
-        import javax.mail.Authenticator;
-        import javax.mail.Message;
-        import javax.mail.MessagingException;
-        import javax.mail.Multipart;
-        import javax.mail.PasswordAuthentication;
-        import javax.mail.Session;
-        import javax.mail.Transport;
-        import javax.mail.internet.InternetAddress;
-        import javax.mail.internet.MimeBodyPart;
-        import javax.mail.internet.MimeMessage;
-        import javax.mail.internet.MimeMultipart;
-        import com.sun.mail.util.MailSSLSocketFactory;
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+import java.io.UnsupportedEncodingException;
+import java.security.GeneralSecurityException;
+import java.util.Date;
+import java.util.Properties;
 
 public class SendEmailUtil {
 
-    private static String account="support@trustnote.org";    //登录用户名
-    private static String pass="ThingTrust123";        //登录密码
-    private static String host="smtp.exmail.qq.com";        //服务器地址（邮件服务器）
-    private static String port="465";        //端口
-    private static String protocol="smtp"; //协议
+    private static final String account = "support@trustnote.org";    //登录用户名
+    private static final String pass = "ThingTrust123";        //登录密码
+    private static final String host = "smtp.exmail.qq.com";        //服务器地址（邮件服务器）
+    private static final String port = "465";        //端口
+    private static final String protocol = "smtp"; //协议
 
     static class MyAuthenricator extends Authenticator{
         String u = null;
         String p = null;
-        public MyAuthenricator(String u,String p){
+
+        public MyAuthenricator(final String u, final String p) {
             this.u=u;
             this.p=p;
         }
         @Override
         protected PasswordAuthentication getPasswordAuthentication() {
-            return new PasswordAuthentication(u,p);
+            return new PasswordAuthentication(this.u, this.p);
         }
     }
 
-    private String to;    //收件人
-    private String subject;    //主题
-    private String content;    //内容
+    private final String to;    //收件人
+    private final String subject;    //主题
+    private final String content;    //内容
     private String fileStr;    //附件路径
 
-    public SendEmailUtil(String to, String subject, String content) {
+    public SendEmailUtil(final String to, final String subject, final String content) {
         this.to = to;
         this.subject = subject;
         this.content = content;
@@ -55,13 +49,13 @@ public class SendEmailUtil {
 
 
     public void send(){
-        Properties prop = new Properties();
+        final Properties prop = new Properties();
         //协议
-        prop.setProperty("mail.transport.protocol", protocol);
+        prop.setProperty("mail.transport.protocol", SendEmailUtil.protocol);
         //服务器
-        prop.setProperty("mail.smtp.host", host);
+        prop.setProperty("mail.smtp.host", SendEmailUtil.host);
         //端口
-        prop.setProperty("mail.smtp.port", port);
+        prop.setProperty("mail.smtp.port", SendEmailUtil.port);
         //使用smtp身份验证
         prop.setProperty("mail.smtp.auth", "true");
         //使用SSL，企业邮箱必需！
@@ -70,50 +64,42 @@ public class SendEmailUtil {
         try {
             sf = new MailSSLSocketFactory();
             sf.setTrustAllHosts(true);
-        } catch (GeneralSecurityException e1) {
+        } catch (final GeneralSecurityException e1) {
             e1.printStackTrace();
         }
         prop.put("mail.smtp.ssl.enable", "true");
         prop.put("mail.smtp.ssl.socketFactory", sf);
 
-        Session session = Session.getDefaultInstance(prop, new MyAuthenricator(account, pass));
+        final Session session = Session.getDefaultInstance(prop, new MyAuthenricator(SendEmailUtil.account, SendEmailUtil.pass));
         session.setDebug(true);
-        MimeMessage mimeMessage = new MimeMessage(session);
+        final MimeMessage mimeMessage = new MimeMessage(session);
         try {
             //发件人
-            mimeMessage.setFrom(new InternetAddress(account,"TrustNote"));        //可以设置发件人的别名
+            mimeMessage.setFrom(new InternetAddress(SendEmailUtil.account, "TrustNote"));
             //mimeMessage.setFrom(new InternetAddress(account));    //如果不需要就省略
             //收件人
-            mimeMessage.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+            mimeMessage.addRecipient(Message.RecipientType.TO, new InternetAddress(this.to));
             //主题
-            mimeMessage.setSubject(subject);
+            mimeMessage.setSubject(this.subject);
             //时间
             mimeMessage.setSentDate(new Date());
             //容器类，可以包含多个MimeBodyPart对象
-            Multipart mp = new MimeMultipart();
+            final Multipart mp = new MimeMultipart();
 
             //MimeBodyPart可以包装文本，图片，附件
-            MimeBodyPart body = new MimeBodyPart();
+            final MimeBodyPart body = new MimeBodyPart();
             //HTML正文
-            body.setContent(content, "text/html; charset=UTF-8");
+            body.setContent(this.content, "text/html; charset=UTF-8");
             mp.addBodyPart(body);
-
-            //添加图片&附件
-//            body = new MimeBodyPart();
-//            body.attachFile(fileStr);
-//            mp.addBodyPart(body);
 
             //设置邮件内容
             mimeMessage.setContent(mp);
             //仅仅发送文本
-            //mimeMessage.setText(content);
             mimeMessage.saveChanges();
             Transport.send(mimeMessage);
-        } catch (MessagingException e) {
+        } catch (final MessagingException e) {
             e.printStackTrace();
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
+        } catch (final UnsupportedEncodingException e) {
             e.printStackTrace();
         }
     }
