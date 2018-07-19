1e332d97d6d846a3cf028eab7202996d193cdc95
wang.li@thingtrust.com <liwang123>
2018-07-19 18:35:25 +0800

    add exchange

--- activity-api/src/main/java/org/trustnote/activity/task/BITZController.java
+++ activity-api/src/main/java/org/trustnote/activity/task/BITZController.java
@@ -1,4 +1,4 @@
-package org.trustnote.activity.controller;
+package org.trustnote.activity.task;
 
 
 import com.alibaba.fastjson.JSONObject;
@@ -7,30 +7,28 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
-@RestController
-@RequestMapping("/expend")
+@Component
+@EnableScheduling
 public class BITZController {
-    private static final Logger logger = LogManager.getLogger(ChannelController.class);
 
-    @RequestMapping(value = "/query-order", method = RequestMethod.GET)
-    public String get() throws IOException {
+    @Scheduled(cron = "0 0/1 * * * ?")
+    public void handleOrders() throws IOException {
         final String url = "https://api.bit-z.com/api_v1/ticker?coin=ttt_btc";
         final HttpGet request = new HttpGet(url);
         // 获取当前客户端对象
-        BITZController.logger.info("开始请求");
         final CloseableHttpClient httpClient = HttpClients.createDefault();
+        System.out.println("开始处理");
         final CloseableHttpResponse httpResponse = httpClient.execute(request);
-        BITZController.logger.info("请求卡顿");
         final String json = EntityUtils.toString(httpResponse.getEntity(), "utf-8");
         final JSONObject jsonObject = (JSONObject) JSONObject.parse(json);
-        return jsonObject.toString();
+        System.out.println("数据" + jsonObject.toJSONString());
     }
+
+
 }
