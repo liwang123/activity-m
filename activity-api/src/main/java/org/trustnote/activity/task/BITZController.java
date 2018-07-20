09fb98d64f82c37ecda5db535a589b38881a9140
zhuxiaolei <xiaolei.zhu@thingtrust.com>
2018-07-20 09:28:02 +0800

    此功能未上线暂时注销.

--- activity-api/src/main/java/org/trustnote/activity/task/BITZController.java
+++ activity-api/src/main/java/org/trustnote/activity/task/BITZController.java
@@ -1,34 +1,34 @@
-package org.trustnote.activity.task;
-
-
-import com.alibaba.fastjson.JSONObject;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.util.EntityUtils;
-import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Component;
-
-import java.io.IOException;
-
-@Component
-@EnableScheduling
-public class BITZController {
-
-    @Scheduled(cron = "0 0/1 * * * ?")
-    public void handleOrders() throws IOException {
-        final String url = "https://api.bit-z.com/api_v1/ticker?coin=ttt_btc";
-        final HttpGet request = new HttpGet(url);
-        // 获取当前客户端对象
-        final CloseableHttpClient httpClient = HttpClients.createDefault();
-        System.out.println("开始处理");
-        final CloseableHttpResponse httpResponse = httpClient.execute(request);
-        final String json = EntityUtils.toString(httpResponse.getEntity(), "utf-8");
-        final JSONObject jsonObject = (JSONObject) JSONObject.parse(json);
-        System.out.println("数据" + jsonObject.toJSONString());
-    }
-
-
-}
+//package org.trustnote.activity.task;
+//
+//
+//import com.alibaba.fastjson.JSONObject;
+//import org.apache.http.client.methods.CloseableHttpResponse;
+//import org.apache.http.client.methods.HttpGet;
+//import org.apache.http.impl.client.CloseableHttpClient;
+//import org.apache.http.impl.client.HttpClients;
+//import org.apache.http.util.EntityUtils;
+//import org.springframework.scheduling.annotation.EnableScheduling;
+//import org.springframework.scheduling.annotation.Scheduled;
+//import org.springframework.stereotype.Component;
+//
+//import java.io.IOException;
+//
+//@Component
+//@EnableScheduling
+//public class BITZController {
+//
+//    @Scheduled(cron = "0 0/1 * * * ?")
+//    public void handleOrders() throws IOException {
+//        final String url = "https://api.bit-z.com/api_v1/ticker?coin=ttt_btc";
+//        final HttpGet request = new HttpGet(url);
+//        // 获取当前客户端对象
+//        final CloseableHttpClient httpClient = HttpClients.createDefault();
+//        System.out.println("开始处理");
+//        final CloseableHttpResponse httpResponse = httpClient.execute(request);
+//        final String json = EntityUtils.toString(httpResponse.getEntity(), "utf-8");
+//        final JSONObject jsonObject = (JSONObject) JSONObject.parse(json);
+//        System.out.println("数据" + jsonObject.toJSONString());
+//    }
+//
+//
+//}
--- activity-api/src/main/java/org/trustnote/activity/task/ExchangeTask.java
+++ activity-api/src/main/java/org/trustnote/activity/task/ExchangeTask.java
@@ -1,53 +1,53 @@
-package org.trustnote.activity.task;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Component;
-import org.trustnote.activity.common.example.ExchangeOrderExample;
-import org.trustnote.activity.common.pojo.ExchangeOrder;
-import org.trustnote.activity.service.iface.ExchangeOrderService;
-import org.trustnote.activity.skeleton.mybatis.mapper.ExchangeOrderMapper;
-
-import java.math.BigDecimal;
-import java.util.List;
-
-/**
- * @author zhuxl
- */
-@Component
-@EnableScheduling
-public class ExchangeTask {
-
-    @Autowired
-    private ExchangeOrderService exchangeOrderService;
-
-    @Autowired
-    private ExchangeOrderMapper exchangeOrderMapper;
-
-    /**
-     * 每隔5分钟处理未支付订单
-     */
-    @Scheduled(cron = "0 0/1 * * * ?")
-    public void handleOrders() {
-        final ExchangeOrderExample exchangeOrderExample = new ExchangeOrderExample();
-        exchangeOrderExample.createCriteria().andStatesEqualTo(1);
-        final List<ExchangeOrder> exchangeOrders = this.exchangeOrderMapper.selectByExample(exchangeOrderExample);
-        System.out.println("测试");
-        exchangeOrders.stream()
-                .forEach(order -> {
-                    //查询order并提出BTC地址是否有钱
-
-                    //如果有钱
-                    order.setReceipt(new BigDecimal(1));
-                    if (order.getReceipt().compareTo(new BigDecimal(0.5)) == -1) {
-                        this.exchangeOrderService.manualMoney(order.getId());
-                    } else {
-                        order.setStates(3);
-                    }
-                    this.exchangeOrderMapper.updateByPrimaryKeySelective(order);
-                });
-    }
-
-
-}
+//package org.trustnote.activity.task;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.scheduling.annotation.EnableScheduling;
+//import org.springframework.scheduling.annotation.Scheduled;
+//import org.springframework.stereotype.Component;
+//import org.trustnote.activity.common.example.ExchangeOrderExample;
+//import org.trustnote.activity.common.pojo.ExchangeOrder;
+//import org.trustnote.activity.service.iface.ExchangeOrderService;
+//import org.trustnote.activity.skeleton.mybatis.mapper.ExchangeOrderMapper;
+//
+//import java.math.BigDecimal;
+//import java.util.List;
+//
+///**
+// * @author zhuxl
+// */
+//@Component
+//@EnableScheduling
+//public class ExchangeTask {
+//
+//    @Autowired
+//    private ExchangeOrderService exchangeOrderService;
+//
+//    @Autowired
+//    private ExchangeOrderMapper exchangeOrderMapper;
+//
+//    /**
+//     * 每隔5分钟处理未支付订单
+//     */
+//    @Scheduled(cron = "0 0/1 * * * ?")
+//    public void handleOrders() {
+//        final ExchangeOrderExample exchangeOrderExample = new ExchangeOrderExample();
+//        exchangeOrderExample.createCriteria().andStatesEqualTo(1);
+//        final List<ExchangeOrder> exchangeOrders = this.exchangeOrderMapper.selectByExample(exchangeOrderExample);
+//        System.out.println("测试");
+//        exchangeOrders.stream()
+//                .forEach(order -> {
+//                    //查询order并提出BTC地址是否有钱
+//
+//                    //如果有钱
+//                    order.setReceipt(new BigDecimal(1));
+//                    if (order.getReceipt().compareTo(new BigDecimal(0.5)) == -1) {
+//                        this.exchangeOrderService.manualMoney(order.getId());
+//                    } else {
+//                        order.setStates(3);
+//                    }
+//                    this.exchangeOrderMapper.updateByPrimaryKeySelective(order);
+//                });
+//    }
+//
+//
+//}
