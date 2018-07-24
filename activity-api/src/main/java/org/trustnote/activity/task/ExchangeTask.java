dc509cabaa45301fd1ef514f57f73203e4235d3d
wang.li@thingtrust.com <liwang123>
2018-07-24 13:35:22 +0800

    upt exchange

--- activity-api/src/main/java/org/trustnote/activity/task/ExchangeTask.java
+++ activity-api/src/main/java/org/trustnote/activity/task/ExchangeTask.java
@@ -1,6 +1,5 @@
 package org.trustnote.activity.task;
 
-import com.alibaba.fastjson.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -12,7 +11,9 @@ import org.trustnote.activity.service.iface.ExchangeOrderService;
 import org.trustnote.activity.skeleton.mybatis.mapper.ExchangeOrderMapper;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author zhuxl
@@ -30,7 +31,7 @@ public class ExchangeTask {
     /**
      * 每隔10分钟处理未支付订单
      */
-    @Scheduled(cron = "0 0/5 * * * ?")
+    @Scheduled(cron = "0 0/2 * * * ?")
     public void handleOrders() {
         final ExchangeOrderExample exchangeOrderExample = new ExchangeOrderExample();
         exchangeOrderExample.createCriteria().andStatesEqualTo(1);
@@ -43,14 +44,16 @@ public class ExchangeTask {
 //                    final String body = OkHttpUtils.get(url, param);
 //                    final JSONObject jsonObject = (JSONObject) JSONObject.parse(body);
 //                    final BigDecimal btcMoney = new BigDecimal(jsonObject.get("data").toString());
-                    final String url = "https://insight.bitpay.com/api/addr/" + order.getToAddress() + "/?noTxList=1";
+                    final String url = "https://testnet.blockchain.info/q/addressbalance/" + order.getToAddress();
                     final String body = OkHttpUtils.get(url, null);
-                    final JSONObject jsonObject = (JSONObject) JSONObject.parse(body);
-                    final BigDecimal btcMoney = new BigDecimal(jsonObject.get("balance").toString());
-                    if (btcMoney.compareTo(new BigDecimal(0.01)) == 1) {
-                        order.setReceipt(btcMoney);
-                        order.setStates(5);
-                        this.exchangeOrderMapper.updateByPrimaryKeySelective(order);
+                    if (body != null) {
+                        final BigDecimal btcMoney = new BigDecimal(body).divide(new BigDecimal(100000000));
+                        if (btcMoney.compareTo(new BigDecimal(0.01)) == 1) {
+                            order.setQuantity(order.getReceipt().divide(order.getRate()));
+                            order.setReceipt(btcMoney);
+                            order.setStates(5);
+                            this.exchangeOrderMapper.updateByPrimaryKeySelective(order);
+                        }
                     }
                 });
     }
@@ -59,24 +62,34 @@ public class ExchangeTask {
     /**
      * 每隔10分钟处理待确认订单
      */
-    @Scheduled(cron = "0 0/10 * * * ?")
+    @Scheduled(cron = "0 0/2 * * * ?")
     public void unconfirmedOrders() {
         final ExchangeOrderExample exchangeOrderExample = new ExchangeOrderExample();
         exchangeOrderExample.createCriteria().andStatesEqualTo(5);
         final List<ExchangeOrder> exchangeOrders = this.exchangeOrderMapper.selectByExample(exchangeOrderExample);
         exchangeOrders.stream()
                 .forEach(order -> {
-                    final String url = "https://insight.bitpay.com/api/addr/" + order.getToAddress() + "/?noTxList=1";
-                    final String body = OkHttpUtils.get(url, null);
-                    final JSONObject jsonObject = (JSONObject) JSONObject.parse(body);
-                    final BigDecimal balance = new BigDecimal(jsonObject.get("balance").toString());
-                    final int unconfirmed_transactions_count = (int) jsonObject.get("unconfirmedBalance");
-                    if (unconfirmed_transactions_count == 0) {
-                        if (balance.compareTo(new BigDecimal(0.05)) == -1) {
-                            this.exchangeOrderService.manualMoney(order.getId());
-                        } else {
-                            this.exchangeOrderMapper.updateByPrimaryKeySelective(order);
-                            this.exchangeOrderService.sendMail(order);
+                    final String url = "https://testnet.blockchain.info/q/addressbalance/" + order.getToAddress();
+                    final Map<String, Integer> param = new HashMap<>();
+                    if (order.getReceipt().compareTo(new BigDecimal(0.05)) == -1) {
+                        param.put("confirmations", 2);
+                        final String body = OkHttpUtils.get(url, param);
+                        if (body != null) {
+                            final BigDecimal balance = new BigDecimal(body);
+                            if (balance.compareTo(order.getReceipt()) != -1) {
+                                this.exchangeOrderService.manualMoney(order.getId());
+                            }
+                        }
+                    } else {
+                        param.put("confirmations", 6);
+                        final String body = OkHttpUtils.get(url, param);
+                        if (body != null) {
+                            final BigDecimal balance = new BigDecimal(body);
+                            if (balance.compareTo(order.getReceipt()) != -1) {
+                                order.setStates(3);
+                                this.exchangeOrderMapper.updateByPrimaryKeySelective(order);
+                                this.exchangeOrderService.sendMail(order);
+                            }
                         }
                     }
                 });
